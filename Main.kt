482d148f5d885e58a725f447343b0c19fc603b7b
K0T1KK <rich050606@gmail.com>
2024-09-23 12:13:25 +0900

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,26 @@
+fun main()
+{
+    //1.10
+    println()
+    println("1.10")
+    val a=50
+    val b=10
+    println("$a\n$b")
+
+    //1.11
+    println()
+    println("1.11")
+    val c=5
+    val e=21
+    println("$c\n$b\n$e")
+
+    //1.12
+    println()
+    println("1.12")
+    println("Введиите 4 числа по отдельности:")
+    val f= readLine()
+    val i= readLine()
+    val s= readLine()
+    val v= readLine()
+    println("Получится:\n$f\n$i\n$s\n$v")
+}
